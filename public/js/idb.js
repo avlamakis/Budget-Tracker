0bc89ae2c79d5e6fc192416e029ff49238c7e51a
alexvlamakis <alexvlamakis@gmail.com>
2022-03-10 18:06:19 -0700

    successful connection to store data on global variable  and local api, version emit will happen with onupgrade, new db also created

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,2 +1,25 @@
 let db;
 const request = indexedDB.open("budget_tracker", 1);
+
+// this event will emit if the database version changes 
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store called `new_budget`, set it to have an auto incrementing primary key 
+    db.createObjectStore('new_budget', { autoIncrement: true });
+  };
+  
+  // upon a successful
+  request.onsuccess = function (event) {
+    // when db is successfully created with its object store save reference and store to a global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run checkDatabase function to send all local db data to api
+    if (navigator.onLine) {
+      uploadBudget();
+    }
+  };
+  
+  request.onerror = function (event) {
+    console.log("Error: " + event.target.errorCode);
+  };
